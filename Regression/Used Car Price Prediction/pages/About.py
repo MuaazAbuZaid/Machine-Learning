50a91c2e45a19eebc41bc1a899ba5bb04c87bcbb
Muaaz Abu Zaid <128296319+MuaazAbuZaid@users.noreply.github.com>
2023-03-21 10:58:15 +0200

    Add files via upload

---
+++ Regression/Used Car Price Prediction/pages/About.py
@@ -0,0 +1,8 @@
+# Importing needed libraries
+import streamlit as st
+
+# Page content
+st.title('About Used Car Price Prediction Project')
+st.write('This app can predict Used Cars Price in egyptian market throughout 10 features affect the price with accuracy about 91.0%.')
+st.write('In "Prediction" page you can enter data for the car you want to know its price.')
+st.write('Note that: the price in the egyptian market isnot stable due to Exchange rate instability.')
