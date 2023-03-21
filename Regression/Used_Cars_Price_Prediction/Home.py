8c71853903d4049cc03ea324d50e7bb7a03cd8a5
Muaaz Abu Zaid <128296319+MuaazAbuZaid@users.noreply.github.com>
2023-03-21 12:11:20 +0200

    Update Home.py

--- Regression/Used_Cars_Price_Prediction/Home.py
+++ Regression/Used_Cars_Price_Prediction/Home.py
@@ -9,4 +9,4 @@ st.markdown(''' <center>  <h6>
     This app is created to predict Used Car Price in Egypt </center> </h6> ''', unsafe_allow_html=True)
 
 # the path of a photo provided from the path of this file
-st.image('./Machine-Learning/Regression/Used_Cars_Price_Prediction/Sources/Cars_Egypt.jpg')
+st.image('./Regression/Used_Cars_Price_Prediction/Sources/Cars_Egypt.jpg')
