75070080e865079923318fbdccba0d46e106a3b5
Muaaz Abu Zaid <128296319+MuaazAbuZaid@users.noreply.github.com>
2023-03-21 11:02:57 +0200

    Update Data.py

--- Regression/Used Car Price Prediction/pages/Data.py
+++ Regression/Used Car Price Prediction/pages/Data.py
@@ -10,7 +10,7 @@ st.markdown(" <center>  <h1> Used Car Dataset </h1> </font> </center> </h1> ",
 st.markdown('<a href="https://www.kaggle.com/datasets/abdo977/used-car-price-in-egypt"> <center> Link to Dataset  </center> </a> ', unsafe_allow_html=True)
 
 # Load data
-df = pd.read_csv('Sources/Cars.csv')
+df = pd.read_csv('./Machine-Learning/Regression/Used Car Price Prediction/Sources/Cars.csv')
 
 # Show data
 st.write('Data collected from a popular website for buying and selling in Egypt. This data contains +14K cars includes 5692 Hyundai, 5033 Fiat and 4016 Chevrolet cars in Egypt.')
